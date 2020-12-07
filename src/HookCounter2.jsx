47621c87eaa9a380805bb09e4d3a45735b432abe
annie2527 <you@example.com>
2020-12-07 12:36:23 -0800

    style

--- src/HookCounter2.jsx
+++ src/HookCounter2.jsx
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import "../src/counter.css";
 
 const HookCounter2 = () => {
     const [count, setCount] = useState(0);
-    const [color, setColor] = useState('');
+    // const [color, setColor] = useState('');
+    const [positive, setPositive] = useState(true);
+    const [style, setStyle] = useState('');
 
     const increment5 = () => {
         setCount(prevCount => prevCount + 5)
@@ -12,9 +15,13 @@ const HookCounter2 = () => {
         setCount(prevCount => prevCount - 5)
     }
 
-    // useEffect(() => {
-    //     setColor(randomColorGenerator())
-    // }, [count]);
+    // const colorCode = () => {
+    //     setColor((count < 0) ? console.log("The number is negative") : console.log("The number is positive");
+    // }
+
+    useEffect(() => {
+        setPositive((count < 0) ? "The number is negative" : "The number is positive");
+    }, [count, style]);
 
     // const randomColorGenerator = () => {
     //     let r = Math.round((Math.random() * 255)); //red 0 to 255
@@ -24,14 +31,15 @@ const HookCounter2 = () => {
     //   };
 
     return (
-        <div>
+        <div className="container">
             <h1>Counter with Hooks + Change by 5s</h1>
-            <h3>{count}</h3>
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <button onClick={() => setCount(count + 1)}>+</button>
+            <h4>{positive}</h4>
+            <h2>Count: {count}</h2>
+            <button className="btn" onClick={() => setCount(count - 1)}>-</button>
+            <button className="btn" onClick={() => setCount(count + 1)}>+</button>
             <br/>
-            <button onClick={decrement5}>Take Away 5</button>
-            <button onClick={increment5}>Add 5</button>
+            <button className="btn five" onClick={decrement5}>Take Away 5</button>
+            <button className="btn five" onClick={increment5}>Add 5</button>
         </div>
     )
 }
